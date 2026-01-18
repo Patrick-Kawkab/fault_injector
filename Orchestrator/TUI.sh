1a1322bdb37c15be137b8230707a65bf57ab1b0f
Stevos19 <stevenghaly2004@gmail.com>
2026-01-18 21:26:27 +0200

    add Mode of injection and input verification

--- Orchestrator/TUI.sh
+++ Orchestrator/TUI.sh
@@ -2,6 +2,16 @@
 
 OUTPUT_FILE=~/Desktop/Input.json
 
+
+INJECTION=$(whiptail --title "Choose Your Injection Mode." \
+--radiolist "Choose your mode" 0 0 0 \
+"HARDWARE" "Real Hardware" ON \
+"QEMU" "Emulated Hardware" OFF 3>&1 1>&2 2>&3)
+
+# Exit if canceled
+[ $? -ne 0 ] && exit 1
+
+
 TARGET=$(whiptail --title "Choose your target." \
 --radiolist "Please choose your target. \n-Hover over your choice and press space to choose. \n-Press enter to confirm choice. \n-Press tab to choose between 'OK' and 'CANCEL' " 0 0 0 \
 "ARM" "ARM Cortex-M" ON \
@@ -33,163 +43,204 @@ for ((i=1; i<=NUM_FAULTS; i++)); do
 
 
 
-
-
-
     if [ "$FAULT" = "BIT FLIP" ]; then
 
-    ADDRESS=$(whiptail --inputbox "Where do you want to inject?" 0 0 --title "Location of injection." 3>&1 1>&2 2>&3)
+    CONFIRMED=false
 
-    # Exit if canceled
-    [ $? -ne 0 ] && exit 1
+        while [ "$CONFIRMED" = false ]; do
 
-    TIME=$(whiptail --inputbox "When do you want to inject? \n-Time in uSec" \
-     0 0 --title "Time of injection." 3>&1 1>&2 2>&3)
-    
-    # Exit if canceled
-    [ $? -ne 0 ] && exit 1
+            ADDRESS=$(whiptail --inputbox "Where do you want to inject?" 0 0 --title "Location of injection." 3>&1 1>&2 2>&3)
 
-    BIT=$(whiptail --inputbox "Which bit?" 0 0 --title "Bit." 3>&1 1>&2 2>&3)
+            # Exit if canceled
+            [ $? -ne 0 ] && exit 1
 
-    # Exit if canceled
-    [ $? -ne 0 ] && exit 1
+            TIME=$(whiptail --inputbox "When do you want to inject? \n-Time in uSec" \
+            0 0 --title "Time of injection." 3>&1 1>&2 2>&3)
+            
+            # Exit if canceled
+            [ $? -ne 0 ] && exit 1
 
-        cat >> "$OUTPUT_FILE" <<EOF
-        {
-            "Target": "$TARGET",
-            "id": $i,
-            "fault_type": "bit_flip",
-            "address": "$ADDRESS",
-            "time": $TIME,
-            "bit": $BIT
-        }
+            BIT=$(whiptail --inputbox "Which bit?" 0 0 --title "Bit." 3>&1 1>&2 2>&3)
+
+            # Exit if canceled
+            [ $? -ne 0 ] && exit 1
+
+            if whiptail --title "Values Confirmation" --yesno "Are you sure of these values? \n Adress: "$ADDRESS" \n Time of injection: $TIME \n Bit: $BIT " 0 0; then
+
+                CONFIRMED=true 
+
+                cat >> "$OUTPUT_FILE" <<EOF
+                {
+                    "Mode": "$INJECTION",
+                    "Target": "$TARGET",
+                    "id": $i,
+                    "fault_type": "bit_flip",
+                    "address": "$ADDRESS",
+                    "time": $TIME,
+                    "bit": $BIT
+                }
 EOF
-    # Add comma except for last entry
-    if [ "$i" -lt "$NUM_FAULTS" ]; then
-        echo "," >> "$OUTPUT_FILE"
+                # Add comma except for last entry
+                if [ "$i" -lt "$NUM_FAULTS" ]; then
+                    echo "," >> "$OUTPUT_FILE"
+                fi
+            else
+                [ $? -eq 255 ] && exit 1
+            fi
+
+        done
+
     fi
-
-
-    fi
-
 
 
 
 
     if [ "$FAULT" = "SENSOR CORRUPTION" ]; then
 
-    SENSOR=$(whiptail --inputbox "Which sensor?" 0 0 --title "Sensor." 3>&1 1>&2 2>&3)
+    CONFIRMED=false
 
-    # Exit if canceled
-    [ $? -ne 0 ] && exit 1
+        while [ "$CONFIRMED" = false ]; do
 
-    MODE=$(whiptail --inputbox "What do you want to do to the sensor?" 0 0 --title "Corruption mode." 3>&1 1>&2 2>&3)
+            SENSOR=$(whiptail --inputbox "Which sensor?" 0 0 --title "Sensor." 3>&1 1>&2 2>&3)
 
-    # Exit if canceled
-    [ $? -ne 0 ] && exit 1
+            # Exit if canceled
+            [ $? -ne 0 ] && exit 1
 
-    VALUE=$(whiptail --inputbox "What value do you want to input?" 0 0 --title "Corruption value." 3>&1 1>&2 2>&3)
+            MODE=$(whiptail --inputbox "What do you want to do to the sensor?" 0 0 --title "Corruption mode." 3>&1 1>&2 2>&3)
 
-    # Exit if canceled
-    [ $? -ne 0 ] && exit 1
+            # Exit if canceled
+            [ $? -ne 0 ] && exit 1
 
-    START_TIME=$(whiptail --inputbox "When do you want to start corruption? \n-Time in uSec" 0 0 --title "Start time." 3>&1 1>&2 2>&3)
+            VALUE=$(whiptail --inputbox "What value do you want to input?" 0 0 --title "Corruption value." 3>&1 1>&2 2>&3)
 
-    # Exit if canceled
-    [ $? -ne 0 ] && exit 1
+            # Exit if canceled
+            [ $? -ne 0 ] && exit 1
 
-    DURATION=$(whiptail --inputbox "How long do you want the corruption to be? \n-Time in uSec" 0 0 --title "Duration." 3>&1 1>&2 2>&3)
+            START_TIME=$(whiptail --inputbox "When do you want to start corruption? \n-Time in uSec" 0 0 --title "Start time." 3>&1 1>&2 2>&3)
 
-    # Exit if canceled
-    [ $? -ne 0 ] && exit 1
+            # Exit if canceled
+            [ $? -ne 0 ] && exit 1
 
-    cat >> "$OUTPUT_FILE" <<EOF
-        {
-            "Target": "$TARGET",
-            "id": $i,
-            "fault_type": "sensor_corruption",
-            "sensor": "$SENSOR",
-            "value": $VALUE,
-            "mode": "$MODE",
-            "start_time": $START_TIME,
-            "duration": $DURATION
-        }
+            DURATION=$(whiptail --inputbox "How long do you want the corruption to be? \n-Time in uSec" 0 0 --title "Duration." 3>&1 1>&2 2>&3)
+
+            # Exit if canceled
+            [ $? -ne 0 ] && exit 1
+
+            if whiptail --title "Values Confirmation" --yesno "Are you sure of these values? \n Sensor: "$SENSOR" \n Mode: $MODE \n Value: $VALUE \n Start time: $START_TIME \n Duration: $DURATION " 0 0; then
+
+                CONFIRMED=true
+
+                    cat >> "$OUTPUT_FILE" <<EOF
+                        {
+                            "Mode": "$INJECTION",
+                            "Target": "$TARGET",
+                            "id": $i,
+                            "fault_type": "sensor_corruption",
+                            "sensor": "$SENSOR",
+                            "value": $VALUE,
+                            "mode": "$MODE",
+                            "start_time": $START_TIME,
+                            "duration": $DURATION
+                        }
 EOF
+                    # Add comma except for last entry
+                    if [ "$i" -lt "$NUM_FAULTS" ]; then
+                        echo "," >> "$OUTPUT_FILE"
+                    fi
+                else
+                    [ $? -eq 255 ] && exit 1
+                fi
 
-    # Add comma except for last entry
-    if [ "$i" -lt "$NUM_FAULTS" ]; then
-        echo "," >> "$OUTPUT_FILE"
+            done
     fi
-
-
-    fi
-
 
 
 
 
     if [ "$FAULT" = "MEMORY CORRUPTION" ]; then
-    
-    ADDRESS=$(whiptail --inputbox "Where do you want to Inject?" 0 0 --title "Location of injection." 3>&1 1>&2 2>&3)
 
-    # Exit if canceled
-    [ $? -ne 0 ] && exit 1
+    CONFIRMED=false
 
-    VALUE=$(whiptail --inputbox "What value do you want to input?" 0 0 --title "Corruption value." 3>&1 1>&2 2>&3)
+        while [ "$CONFIRMED" = false ]; do
+        
+            ADDRESS=$(whiptail --inputbox "Where do you want to Inject?" 0 0 --title "Location of injection." 3>&1 1>&2 2>&3)
 
-    # Exit if canceled
-    [ $? -ne 0 ] && exit 1
+            # Exit if canceled
+            [ $? -ne 0 ] && exit 1
 
-    cat >> "$OUTPUT_FILE" <<EOF
-        {
-            "Target": "$TARGET",
-            "id": $i,
-            "fault_type": "memory_corruption",
-            "address": "$ADDRESS",
-            "value": $VALUE
-        }
+            VALUE=$(whiptail --inputbox "What value do you want to input?" 0 0 --title "Corruption value." 3>&1 1>&2 2>&3)
+
+            # Exit if canceled
+            [ $? -ne 0 ] && exit 1
+
+            if whiptail --title "Values Confirmation" --yesno "Are you sure of these values? \n Adress: "$ADDRESS" \n Value: $VALUE" 0 0; then
+
+                CONFIRMED=true 
+
+                cat >> "$OUTPUT_FILE" <<EOF
+                    {
+                        "Mode": "$INJECTION",
+                        "Target": "$TARGET",
+                        "id": $i,
+                        "fault_type": "memory_corruption",
+                        "address": "$ADDRESS",
+                        "value": $VALUE
+                    }
 EOF
 
-    # Add comma except for last entry
-    if [ "$i" -lt "$NUM_FAULTS" ]; then
-        echo "," >> "$OUTPUT_FILE"
+                # Add comma except for last entry
+                if [ "$i" -lt "$NUM_FAULTS" ]; then
+                    echo "," >> "$OUTPUT_FILE"
+                fi
+            else
+                [ $? -eq 255 ] && exit 1
+            fi
+            
+        done
+
     fi
-
-
-    fi
-
-
-
 
 
 
     if [ "$FAULT" = "SET PC" ]; then
 
-    JUMP_TIME=$(whiptail --inputbox "When do you want to jump? \n-Time in uSec" 0 0 --title "Jump time." 3>&1 1>&2 2>&3)
+    CONFIRMED=false
 
-    # Exit if canceled
-    [ $? -ne 0 ] && exit 1
+        while [ "$CONFIRMED" = false ]; do
 
-    LOCATION=$(whiptail --inputbox "Where do you want to jump?" 0 0 --title "Jumping location." 3>&1 1>&2 2>&3)
+            JUMP_TIME=$(whiptail --inputbox "When do you want to jump? \n-Time in uSec" 0 0 --title "Jump time." 3>&1 1>&2 2>&3)
 
-    # Exit if canceled
-    [ $? -ne 0 ] && exit 1
+            # Exit if canceled
+            [ $? -ne 0 ] && exit 1
 
-    cat >> "$OUTPUT_FILE" <<EOF
-        {
-            "Target": "$TARGET",
-            "id": $i,
-            "fault_type": "setpc",
-            "time": "$JUMP_TIME",
-            "location": "$LOCATION"
-        }
+            LOCATION=$(whiptail --inputbox "Where do you want to jump?" 0 0 --title "Jumping location." 3>&1 1>&2 2>&3)
+
+            # Exit if canceled
+            [ $? -ne 0 ] && exit 1
+
+            if whiptail --title "Values Confirmation" --yesno "Are you sure of these values? \n Time of jumping: "$JUMP_TIME" \n Location: $LOCATION " 0 0; then
+
+                CONFIRMED=true 
+
+                cat >> "$OUTPUT_FILE" <<EOF
+                    {
+                        "Mode": "$INJECTION",
+                        "Target": "$TARGET",
+                        "id": $i,
+                        "fault_type": "setpc",
+                        "time": "$JUMP_TIME",
+                        "location": "$LOCATION"
+                    }
 EOF
 
-    # Add comma except for last entry
-    if [ "$i" -lt "$NUM_FAULTS" ]; then
-        echo "," >> "$OUTPUT_FILE"
-    fi
-
+                # Add comma except for last entry
+                if [ "$i" -lt "$NUM_FAULTS" ]; then
+                    echo "," >> "$OUTPUT_FILE"
+                fi
+            else
+                [ $? -eq 255 ] && exit 1
+            fi
+        done
 
     fi
 
