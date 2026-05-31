f30e0436c5e7ef8fe30316911a73ef1f9c6f1e36
ZyadRizk <zyadh3545@gmail.com>
2026-05-31 20:28:59 +0300

    refactor: reorganize Session class methods and update signatures for Fault Descriptor

--- Session.h
+++ Session.h
@@ -3,27 +3,28 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <memory>
+#include <cstdint>
 #include <unistd.h>
 #include <signal.h>
-#include <fstream>
 #include <sys/wait.h>
-#include <cstdint>
-#include <memory>
+#include "FaultConfig.h"
 
 class Session {
 public:
     virtual ~Session()= default;
+
     virtual int start()=0;
-    virtual int stop()=0;
-    virtual int setPC(uint32_t pc)=0;
-    virtual bool memoryCorruptionTest(uint32_t addr,uint8_t injectedValue,uint8_t minExpected,  uint8_t maxExpected)=0;
-    bool bitFlipTest(uint32_t addr,   uint8_t  bitPos,
-                     uint8_t  minExp, uint8_t  maxExp,
-                     uint64_t targetCount);    static std::unique_ptr<Session>create(const std::string& type);
-    bool instructionSkipTest(uint32_t addr);
-    bool sensorCorruptionTest(uint32_t sensorAddr,
-                              uint8_t  spoofedValue,
-                              uint8_t  minExpected,
-                              uint8_t  maxExpected);
+    virtual int stop() noexcept=0;
+
+    virtual FaultResult setPC(const FaultDescriptor& desc)=0;
+    virtual FaultResult memoryCorruptionTest(const FaultDescriptor& desc)=0;
+    virtual FaultResult bitFlipTest(const FaultDescriptor& desc)=0;    
+    virtual FaultResult instructionSkipTest(uint32_t addr)=0;
+    virtual FaultResult sensorCorruptionTest(const FaultDescriptor& desc)=0;
+    
+    static std::unique_ptr<Session> create(const std::string& type);
 };
+    
 #endif
